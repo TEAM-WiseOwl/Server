83b2ae2af2d52794ab864b05e1e9f79bc94f582d
largerejade <lsj31200@naver.com>
2024-11-09 12:32:44 +0900

    [#7] accounts models,py import 추가

--- accounts/models.py
+++ accounts/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
-
+from django.utils.translation import gettext_lazy as _
 
 class UserManager(BaseUserManager):
     def create_user(self, email, password, **extra_fields):
@@ -33,15 +33,27 @@ class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(max_length=50, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
     is_staff = models.BooleanField(default=False)
-    major_college = models.ForeignKey(College, verbose_name='대학', on_delete= models.CASCADE, related_name='major_college_id')
-    double_or_minor_college = models.ForeignKey(College, verbose_name='대학', on_delete= models.CASCADE, related_name='double_or_minor_college_id')
-    major = models.ForeignKey(Department, verbose_name='학과', on_delete= models.CASCADE, related_name='major_id')
-    double_or_minor = models.ForeignKey(Department, verbose_name='학과', on_delete= models.CASCADE, related_name='double_or_minor_id')
+    is_active = models.BooleanField(default=True)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+    
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True, related_name='profile')
+    major_college = models.ForeignKey(College, verbose_name='대학', on_delete=models.CASCADE, related_name='major_college')
+    double_or_minor_college = models.ForeignKey(College, verbose_name='이중/부전공 대학', on_delete=models.CASCADE, related_name='double_minor_college')
+    major = models.ForeignKey(Department, verbose_name='학과', on_delete=models.CASCADE, related_name='major')
+    double_or_minor = models.ForeignKey(Department, verbose_name='이중/부전공 학과', on_delete=models.CASCADE, related_name='double_minor')
     profile_name = models.CharField(max_length=20, unique=True)
     profile_student_number = models.PositiveIntegerField(verbose_name='학번')
     profile_gubun = models.CharField(max_length=20)
     profile_agreement = models.BooleanField(default=False)
-    profile_img = models.ImageField(upload_to="profiles/%Y/%m/%d/")
+    profile_img = models.ImageField(upload_to="profiles/%Y/%m/%d/", blank=True, null=True)
     updated_at = models.DateTimeField(auto_now=True, null=True, blank=True)
     profile_grade = models.FloatField(default=0)
     grad_research = models.BooleanField(default=False)
@@ -52,10 +64,5 @@ class User(AbstractBaseUser, PermissionsMixin):
     for_language_name = models.CharField(max_length=50, verbose_name='외국어 시험명', blank=True, null=True)
     for_language_score = models.PositiveBigIntegerField(verbose_name='외국어 시험점수', blank=True, null=True)
 
-    objects = UserManager()
-
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
-
     def __str__(self):
-        return self.email
+        return f"{self.user.email} - Profile"
