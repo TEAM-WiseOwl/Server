650cc95dc7f12e5afa26b9cbd19ff784db9bc0dc
largerejade <lsj31200@naver.com>
2024-11-15 21:57:53 +0900

    [#16] 구글 로그인 기능 구현

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path, include
+from accounts import views
 from .views import *
 
 app_name = 'accounts'
 
 urlpatterns = [
-
+    path('google/login/', views.google_login, name='google_login'),
+    path('google/callback/', views.google_callback, name='google_callback'),  
+    path('google/login/finish/', views.GoogleLogin.as_view(), name='google_login_todjango'),
+    #path('google/token/refresh/ ', TokenRefreshAPIView.as_view(), name='token_refresh')
 ]
