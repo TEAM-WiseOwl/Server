b5e5cc851bc12744628a629c23cee3972f83ffb8
onlyone020608 <onlyone0608@hufs.ac.kr>
2024-11-16 20:48:09 +0900

    [#15] notices 앱 crawler 코드 draft 작성완-ver notices view에다가 그냥 마이페이지 view를 일단 만듬..

--- notices/serializers.py
+++ notices/serializers.py
@@ -1,13 +1,104 @@
 from rest_framework import serializers
 from accounts.models import *
 from requirements.models import *
+from collections import defaultdict
 
-class MypageSerializezr(serializers.ModelSerializer):
-  major = serializers.SerializerMethodField()
+class MypageSerializer(serializers.ModelSerializer):
+  department_name = serializers.SerializerMethodField()
+  double_minor_department_name = serializers.SerializerMethodField()
   class Meta:
     model = Profile
-    fields = ['profile_name', 'profile_student_number', 'profile_grade', 'major']
+    fields = ['profile_name', 'profile_student_number', 'profile_grade', 'major','double_major']
   
-  def get_department_name(self, obj):
-    return obj.department.department_name
+  def get_major(self, obj):
+    major_department = Department.objects.get(department_id=obj.major_id)
+    return major_department.department_name
+  def get_double_major(self, obj):
+    double_minor_department = Department.objects.get(department_id=obj.double_minor_id)
+    return double_minor_department.department_name
+  
+  class MyInfoSerializer(serializers.ModelSerializer):
+    college = serializers.SerializerMethodField()
+    major = serializers.SerializerMethodField()
+    double_college = serializers.SerializerMethodField()
+    double_major = serializers.SerializerMethodField()
+    class Meta:
+      model = Profile
+      field=['college', 'major', 'double_college', 'double_major', 'profile_gubun' ]
+
+    def get_college(self, obj):
+      major_college = College.objects.get(college_id=obj.major_college_id)
+      return major_college.college_name
+    def get_major(self, obj):
+      major = Department.objects.get(department_id=obj.major_id)
+      return major.department_name
+    def get_double_college(self, obj):
+      double_college=College.objects.get(college_id=obj.double_or_minor_college_id)
+      return double_college.college_name
     
+    def get_double_major(self, obj):
+      double_major=Department.objects.get(department_id=obj.double_or_minor_id)
+      return double_major.department_name
+    
+
+class CourseSubjectSerializer(serializers.Serializer):
+    subject_name = serializers.CharField()
+    grade = serializers.CharField()
+    retry_yn = serializers.BooleanField()
+    credit = serializers.IntegerField()
+
+class CourseYearSerializer(serializers.Serializer):
+    complete_year = serializers.CharField()
+    school_year = serializers.IntegerField()
+    course_subject = CourseSubjectSerializer(many=True)
+
+class CourseCompleteSerializer(serializers.ModelSerializer):
+  double_major = serializers.SerializerMethodField()
+  major = serializers.SerializerMethodField()
+  course = serializers.SerializerMethodField()
+  class Meta:
+    field=['major','profile_gubun', 'second_major', 'complete_year', 'course' ]
+  def get_major(self, obj):
+      major = Department.objects.get(department_id=obj.major_id)
+      return major.department_name
+  def get_double_major(self, obj):
+      double_major=Department.objects.get(department_id=obj.double_or_minor_id)
+      return double_major.department_name
+  def get_course(self, obj):
+        major_subjects = MajorSubjectCompleted.objects.filter(user=obj.user)
+        general_subjects = GeneralSubjectCompleted.objects.filter(user=obj.user)
+
+        # 결과를 그룹화할 dict 초기화
+        courses_by_year = defaultdict(lambda: {
+            'complete_year': None,
+            'school_year': None,
+            'course_subject': []
+        })
+
+        # MajorSubjectCompleted 모델을 complete_year, school_year로 묶어서 데이터 준비
+        for major_subject in major_subjects:
+            key = (major_subject.completed_year, major_subject.school_year)
+            courses_by_year[key]['complete_year'] = major_subject.completed_year
+            courses_by_year[key]['school_year'] = major_subject.school_year
+            courses_by_year[key]['course_subject'].append({
+                'subject_name': major_subject.subject_department.subject_name,
+                'grade': major_subject.grade,
+                'retry_yn': major_subject.retry_yn,
+                'credit': major_subject.subject_department.credit
+            })
+
+        # GeneralSubjectCompleted 모델을 complete_year, school_year로 묶어서 데이터 준비
+        for general_subject in general_subjects:
+            key = (general_subject.completed_year, general_subject.school_year)
+            courses_by_year[key]['complete_year'] = general_subject.completed_year
+            courses_by_year[key]['school_year'] = general_subject.school_year
+            courses_by_year[key]['course_subject'].append({
+                'subject_name': general_subject.subject_gened.subject_name,
+                'grade': general_subject.grade,
+                'retry_yn': general_subject.retry_yn,
+                'credit': general_subject.subject_gened.credit
+            })
+
+        # courses_by_year를 리스트로 반환
+        return [CourseYearSerializer(data=course).data for course in courses_by_year.values()]
+
--- notices/views.py
+++ notices/views.py
@@ -7,6 +7,7 @@ from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
 from accounts.models import *
 from .serializers import *
+from rest_framework import status
 
 
 
@@ -23,7 +24,98 @@ class Mypage(APIView):
   permission_classes = [AllowAny]
   authentication_classes = []
   def get(self,request, user_id):
-    print(user_id)
+    print(Profile.objects.all())
     profile = Profile.objects.get(user=user_id)
-    serializer = MypageSerializezr(profile)
+    serializer = MypageSerializer(profile)
     return Response(serializer.data)
+
+class MyinfoEdit(APIView):
+  def get(self, request, user_id):
+    profile = Profile.objects.get(user=user_id)
+    serializer = MypageSerializer(profile)
+    return Response(serializer.data)
+  
+class MyCourseEdit(APIView):
+  def get(self, request, user_id):
+    profile=Profile.objects.get(user=user_id)
+    serializers=CourseCompleteSerializer(profile)
+    return Response(serializers.data)
+  def delete(self, request):
+    complete_year = request.data.get("complete_year")
+    school_year = request.data.get("school_year")
+    subject_name = request.data.get("subject_name")
+    #major 테이블에서 찾음
+    subject_department = SubjectDepartment.objects.filter(subject_name=subject_name).first()
+    if subject_department:
+      major_subject = MajorSubjectCompleted.objects.filter(
+        user=request.user,
+        subject_department=subject_department,
+        completed_year=complete_year,
+        school_year=school_year
+        ).first()
+
+        # 과목이 존재하면 삭제
+      if major_subject:
+        major_subject.delete()
+        return Response({
+                    "message": "Resource deleted successfully"
+                }, status=status.HTTP_204_NO_CONTENT)
+    #교양 테이블
+    subject_gened = SubjectGened.objects.filter(subject_name=subject_name).first()
+    if subject_gened:
+      general_subject = GeneralSubjectCompleted.objects.filter(
+                user=request.user,
+                subject_gened=subject_gened,
+                completed_year=complete_year,
+                school_year=school_year
+            ).first()
+
+            # 과목이 존재하면 삭제
+      if general_subject:
+                general_subject.delete()
+                return Response({
+                    "message": "Resource deleted successfully"
+                }, status=status.HTTP_204_NO_CONTENT)
+
+
+  def patch(self, request):
+    complete_year = request.data.get("complete_year")
+    school_year = request.data.get("school_year")
+    subject_name = request.data.get("subject_name")
+    grade = request.data.get("grade")
+    retry_yn = request.data.get("retry_yn")
+
+    #전공테이블 찾음
+    subject_department = SubjectDepartment.objects.filter(subject_name=subject_name).first()
+    if subject_department:
+      major_subject = MajorSubjectCompleted.objects.filter(
+      user=request.user,
+      subject_department=subject_department,
+      completed_year=complete_year,
+      school_year=school_year
+        ).first()
+      if major_subject:
+        major_subject.grade = grade
+        major_subject.retry_yn = retry_yn
+        major_subject.save()
+        return Response({
+                        "message": "Resource updated successfully"
+                    }, status=status.HTTP_200_OK)
+    subject_gened = SubjectGened.objects.filter(subject_name=subject_name).first()
+    if subject_gened:
+      general_subject = GeneralSubjectCompleted.objects.filter(
+        user=request.user,
+        subject_gened=subject_gened,
+        completed_year=complete_year,
+        school_year=school_year
+          ).first()
+      if general_subject:
+        general_subject.grade = grade
+        general_subject.retry_yn = retry_yn
+        general_subject.save()
+        return Response({
+                    "message": "Resource updated successfully"
+                }, status=status.HTTP_200_OK)
+    # 과목이 존재하지 않으면 오류 반환
+      return Response({"detail": "Subject not found."}, status=status.HTTP_404_NOT_FOUND)
+    
