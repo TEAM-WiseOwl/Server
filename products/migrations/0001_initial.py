b8eb0a4dc42902f3dc98d55440aa466e618e4a2a
박주영 <143913325+Juyoung1520@users.noreply.github.com>
2024-11-18 09:10:46 +0900

    [#8] initial.py 맞춤

--- products/migrations/0001_initial.py
+++ products/migrations/0001_initial.py
@@ -9,8 +9,8 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
         ("facilities", "0001_initial"),
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
     operations = [
