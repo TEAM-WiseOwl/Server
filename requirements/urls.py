f000487be918dd42bc456ea93cd11d070c912f4e
largerejade <lsj31200@naver.com>
2024-11-20 04:25:02 +0900

    [#32] 학과 및 교양 과목 구분 정보 api 기능 구현

--- requirements/urls.py
+++ requirements/urls.py
@@ -6,4 +6,5 @@ app_name = 'requirements'
 urlpatterns = [
     path('colleges/', CollegeListAPIView.as_view(), name=''),
     path('colleges/<str:subject_semester>/division/', SubjectDivisionAPIView.as_view()),
+    path('colleges/<str:subject_semester>/subjects/', SubjectAPIView.as_view()),
 ]
